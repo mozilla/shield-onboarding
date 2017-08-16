6bc40e9812f792ddbe2c46650d1f50059a31ab96
Rex Lee <rexboy@mozilla.com>
2017-08-16 18:45:53 +0800

    Merge bootstrap.js of Onboarding and Shield extension

--- addon/bootstrap.js
+++ addon/bootstrap.js
@@ -4,7 +4,17 @@
 /* global  __SCRIPT_URI_SPEC__  */
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "(startup|shutdown|install|uninstall)" }]*/
 
-const {utils: Cu} = Components;
+const {utils: Cu, interfaces: Ci} = Components;
+/* Onobarding */
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "OnboardingTourType",
+  "resource://onboarding/modules/OnboardingTourType.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "Services",
+  "resource://gre/modules/Services.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "fxAccounts",
+  "resource://gre/modules/FxAccounts.jsm");
+
+/* Shield */
 const CONFIGPATH = `${__SCRIPT_URI_SPEC__}/../Config.jsm`;
 const { config } = Cu.import(CONFIGPATH, {});
 const studyConfig = config.study;
@@ -13,6 +23,180 @@ const log = createLog(studyConfig.studyName, config.log.bootstrap.level);  // de
 
 const STUDYUTILSPATH = `${__SCRIPT_URI_SPEC__}/../${studyConfig.studyUtilsPath}`;
 const { studyUtils } = Cu.import(STUDYUTILSPATH, {});
+
+/* Onboarding functions and constants */
+const {PREF_STRING, PREF_BOOL, PREF_INT} = Ci.nsIPrefBranch;
+
+const BROWSER_READY_NOTIFICATION = "browser-delayed-startup-finished";
+const BROWSER_SESSION_STORE_NOTIFICATION = "sessionstore-windows-restored";
+const PREF_WHITELIST = [
+  ["browser.onboarding.enabled", PREF_BOOL],
+  ["browser.onboarding.hidden", PREF_BOOL],
+  ["browser.onboarding.notification.finished", PREF_BOOL],
+  ["browser.onboarding.notification.prompt-count", PREF_INT],
+  ["browser.onboarding.notification.last-time-of-changing-tour-sec", PREF_INT],
+  ["browser.onboarding.notification.tour-ids-queue", PREF_STRING],
+];
+
+[
+  "onboarding-tour-addons",
+  "onboarding-tour-customize",
+  "onboarding-tour-default-browser",
+  "onboarding-tour-library",
+  "onboarding-tour-performance",
+  "onboarding-tour-private-browsing",
+  "onboarding-tour-search",
+  "onboarding-tour-singlesearch",
+  "onboarding-tour-sync",
+].forEach(tourId => PREF_WHITELIST.push([`browser.onboarding.tour.${tourId}.completed`, PREF_BOOL]));
+
+let waitingForBrowserReady = true;
+
+/**
+ * Set pref. Why no `getPrefs` function is due to the priviledge level.
+ * We cannot set prefs inside a framescript but can read.
+ * For simplicity and effeciency, we still read prefs inside the framescript.
+ *
+ * @param {Array} prefs the array of prefs to set.
+ *   The array element carrys info to set pref, should contain
+ *   - {String} name the pref name, such as `browser.onboarding.hidden`
+ *   - {*} value the value to set
+ **/
+function setPrefs(prefs) {
+  prefs.forEach(pref => {
+    let prefObj = PREF_WHITELIST.find(([name, ]) => name == pref.name);
+    if (!prefObj) {
+      return;
+    }
+
+    let [name, type] = prefObj;
+
+    switch (type) {
+      case PREF_BOOL:
+        Services.prefs.setBoolPref(name, pref.value);
+        break;
+
+      case PREF_INT:
+        Services.prefs.setIntPref(name, pref.value);
+        break;
+
+      case PREF_STRING:
+        Services.prefs.setStringPref(name, pref.value);
+        break;
+
+      default:
+        throw new TypeError(`Unexpected type (${type}) for preference ${name}.`)
+    }
+  });
+}
+
+/**
+ * syncTourChecker listens to and maintains the login status inside, and can be
+ * queried at any time once initialized.
+ */
+let syncTourChecker = {
+  _registered: false,
+  _loggedIn: false,
+
+  isLoggedIn() {
+    return this._loggedIn;
+  },
+
+  observe(subject, topic) {
+    switch (topic) {
+      case "fxaccounts:onlogin":
+        this.setComplete();
+        break;
+      case "fxaccounts:onlogout":
+        this._loggedIn = false;
+        break;
+    }
+  },
+
+  init() {
+    // Check if we've already logged in at startup.
+    fxAccounts.getSignedInUser().then(user => {
+      if (user) {
+        this.setComplete();
+      }
+      // Observe for login action if we haven't logged in yet.
+      this.register();
+    });
+  },
+
+  register() {
+    if (this._registered) {
+      return;
+    }
+    Services.obs.addObserver(this, "fxaccounts:onlogin");
+    Services.obs.addObserver(this, "fxaccounts:onlogout");
+    this._registered = true;
+  },
+
+  setComplete() {
+    this._loggedIn = true;
+    Services.prefs.setBoolPref("browser.onboarding.tour.onboarding-tour-sync.completed", true);
+  },
+
+  unregister() {
+    if (!this._registered) {
+      return;
+    }
+    Services.obs.removeObserver(this, "fxaccounts:onlogin");
+    Services.obs.removeObserver(this, "fxaccounts:onlogout");
+    this._registered = false;
+  },
+
+  uninit() {
+    this.unregister();
+  },
+}
+
+/**
+ * Listen and process events from content.
+ */
+function initContentMessageListener() {
+  Services.mm.addMessageListener("Onboarding:OnContentMessage", msg => {
+    switch (msg.data.action) {
+      case "set-prefs":
+        setPrefs(msg.data.params);
+        break;
+      case "get-login-status":
+        msg.target.messageManager.sendAsyncMessage("Onboarding:ResponseLoginStatus", {
+          isLoggedIn: syncTourChecker.isLoggedIn()
+        });
+        break;
+    }
+  });
+}
+
+/**
+ * onBrowserReady - Continues startup of the add-on after browser is ready.
+ */
+function onBrowserReady() {
+  waitingForBrowserReady = false;
+
+  OnboardingTourType.check();
+  Services.mm.loadFrameScript("resource://onboarding/onboarding.js", true);
+  initContentMessageListener();
+}
+
+/**
+ * observe - nsIObserver callback to handle various browser notifications.
+ */
+function observe(subject, topic, data) {
+  switch (topic) {
+    case BROWSER_READY_NOTIFICATION:
+      Services.obs.removeObserver(observe, BROWSER_READY_NOTIFICATION);
+      onBrowserReady();
+      break;
+    case BROWSER_SESSION_STORE_NOTIFICATION:
+      Services.obs.removeObserver(observe, BROWSER_SESSION_STORE_NOTIFICATION);
+      syncTourChecker.init();
+      break;
+  }
+}
+
 
 async function startup(addonData, reason) {
   // addonData: Array [ "id", "version", "installPath", "resourceURI", "instanceID", "webExtension" ]  bootstrap.js:48
@@ -52,6 +236,36 @@ async function startup(addonData, reason) {
     //  other message handlers from your addon, if any
   });
   // studyUtils.endStudy("user-disable");
+
+  /* Onboarding works */
+  if (reason === ADDON_INSTALL) {
+
+    // Preferences for Photon onboarding system extension
+    Services.prefs.setBoolPref("browser.onboarding.enabled", true);
+    // Mark this as an upgraded profile so we don't offer the initial new user onboarding tour.
+    Services.prefs.setIntPref("browser.onboarding.tourset-version", 1);
+    Services.prefs.setBoolPref("browser.onboarding.hidden", false);
+    // On the Activity-Stream page, the snippet's position overlaps with our notification.
+    // So use `browser.onboarding.notification.finished` to let the AS page know
+    // if our notification is finished and safe to show their snippet.
+    Services.prefs.setBoolPref("browser.onboarding.notification.finished", false);
+    Services.prefs.setIntPref("browser.onboarding.notification.mute-duration-on-first-session-ms", 300000); // 5 mins
+    Services.prefs.setIntPref("browser.onboarding.notification.max-life-time-per-tour-ms", 432000000); // 5 days
+    Services.prefs.setIntPref("browser.onboarding.notification.max-prompt-count-per-tour", 8);
+    Services.prefs.setStringPref("browser.onboarding.newtour", "private,addons,customize,search,default,sync");
+    Services.prefs.setStringPref("browser.onboarding.updatetour", "");
+
+    // Preference that allows individual users to disable Screenshots.
+    Services.prefs.setBoolPref("extensions.screenshots.disabled", false);
+  }
+  // Only start Onboarding when the browser UI is ready
+  if (Services.startup.startingUp) {
+    Services.obs.addObserver(observe, BROWSER_READY_NOTIFICATION);
+    Services.obs.addObserver(observe, BROWSER_SESSION_STORE_NOTIFICATION);
+  } else {
+    onBrowserReady();
+    syncTourChecker.init();
+  }
 }
 
 
@@ -73,6 +287,13 @@ function shutdown(addonData, reason) {
     Jsm.unload(config.modules);
     Jsm.unload([CONFIGPATH, STUDYUTILSPATH]);
   }
+
+  /* Onboarding works */
+  // Stop waiting for browser to be ready
+  if (waitingForBrowserReady) {
+    Services.obs.removeObserver(observe, BROWSER_READY_NOTIFICATION);
+  }
+  syncTourChecker.uninit();
 }
 
 function uninstall(addonData, reason) {
